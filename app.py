5cbe38fb8fb596cd6977ad5df8e04147b528cf6d
Kelsey Kuno <kelsey.kuno@gmail.com>
2023-01-24 11:31:59 -0600

    double loop, printed shape and largest ex. completed

--- app.py
+++ app.py
@@ -47,19 +47,47 @@ import math
 
 # Car game:
 
-user_command = ''
+# user_command = ''
 
 
-while user_command.lower() != 'quit':
-    user_command = input('what is your command ')
-    if user_command.lower() == 'start':
-        print('vroom....car started')
-    elif user_command.lower() == 'stop':
-        print('errrrrr stop car')
-    elif user_command.lower() == 'quit':
-        print('goodbye')
-else:
-    print('you quit')
+# while user_command.lower() != 'quit':
+#     user_command = input('what is your command ')
+#     if user_command.lower() == 'start':
+#         print('vroom....car started')
+#     elif user_command.lower() == 'stop':
+#         print('errrrrr stop car')
+#     elif user_command.lower() == 'quit':
+#         print('goodbye')
+# else:
+#     print('you quit')
+
+# shopping cart calculation
+# prices = [10, 20, 40]
+# total = 0
+
+# for item in prices:
+#     print(item)
+#     total = total + item
+
+# print(total)
+
+# draw an f
+# shape = [5, 2, 5, 2, 2]
+
+# for number in shape:
+#     print("x" * number)
+
+# find largest number 
+
+# list = [0, 5,9, 1000]
+# first_number = list[0]
+
+# for number in list:
+#     if first_number >= number:
+#         first_number = first_number
+#     else: 
+#         first_number = number
+# print(f'this  is the largest number{first_number}')
 
 
 
